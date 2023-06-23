bb9fb6e90c69d8e441e2c99b132934ec6b3d13ef
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-06-23 15:56:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "instance_name" {
+  type  = string
+  default  = "kali-withoutSoft"
+}
+
+variable "instance_name2" {
+  type  = string
+  default  = "kaliLinux_demo"
+}
+
+variable "instance_name3" {
+  type  = string
+  default  = "Metasploitable-vagrant"
+}
+
+variable "instance_name4" {
+  type  = string  
+  default  = "Windows-10-Pro"
+}
+
+variable "instance_name5" {
+  type  = string
+  default  = "marlinspike"
+}
