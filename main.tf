2f98d4b4f8ddd6aee3ed1c29d59258a8fb66f0d7
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-06-23 15:55:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+variable "instance_name" {
+  type  = string
+  default  = "kali-withoutSoft"
+}
+
+variable "instance_name2" {
+  type  = string
+  default  = "kaliLinux_demo"
+}
+
+variable "instance_name3" {
+  type  = string
+  default  = "Metasploitable-vagrant"
+}
+
+variable "instance_name4" {
+  type  = string  
+  default  = "Windows-10-Pro"
+}
+
+variable "instance_name5" {
+  type  = string
+  default  = "marlinspike"
+}
+
 provider "aws" {
   region = "ap-southeast-1"  # Replace with your desired AWS region
 }
