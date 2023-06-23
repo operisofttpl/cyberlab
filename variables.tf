21bc3cbe0f8831ac7af098df544146b06a33c5b1
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-06-23 15:48:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,24 @@
 variable "instance_name" {
   type    = string
-  description = "The name to give the EC2 instance"
   default     = "kali-withoutSoft"
 }
 
 variable "instance_name2" {
   type    = string
-  description = "The name to give the EC2 instance"
   default     = "kaliLinux_demo"
 }
 
 variable "instance_name3" {
   type    = string
-  description = "The name to give the EC2 instance"
   default     = "Metasploitable-vagrant"
 }
 
 variable "instance_name4" {
-  type    = string
-  description = "The name to give the EC2 instance"
+  type    = string  
   default     = "Windows-10-Pro"
 }
 
 variable "instance_name5" {
   type    = string
-  description = "The name to give the EC2 instance"
   default     = "marlinspike"
 }
