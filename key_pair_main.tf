2d95e743756b6245f13c612c950631e4f7806e01
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-06-23 16:20:19 +0530

    Update key_pair_main.tf

--- key_pair_main.tf
+++ key_pair_main.tf
@@ -11,12 +11,6 @@ resource "aws_key_pair" "kali-without-key-pair" {
   public_key = tls_private_key.kali-without-key-pair.public_key_openssh
 }
 
-# Save file
-resource "local_file" "ssh_key" {
-  filename = "${aws_key_pair.kali-without-key-pair.key_name}.pem"
-  content  = tls_private_key.kali-without-key-pair.private_key_pem
-}
-
 # Generates a secure private key and encodes it as PEM
 # 2 key
 resource "tls_private_key" "marlinspike-key-pair" {
@@ -28,12 +22,6 @@ resource "tls_private_key" "marlinspike-key-pair" {
 resource "aws_key_pair" "marlinspike-key-pair" {
   key_name   = "marlinspike-key-pair"  
   public_key = tls_private_key.marlinspike-key-pair.public_key_openssh
-}
-
-# Save file
-resource "local_file" "marlinspike-key-pair" {
-  filename = "${aws_key_pair.marlinspike-key-pair.key_name}.pem"
-  content  = tls_private_key.marlinspike-key-pair.private_key_pem
 }
 
 # Generates a secure private key and encodes it as PEM
@@ -49,12 +37,6 @@ resource "aws_key_pair" "Metasploitable-vagrant-key-pair" {
   public_key = tls_private_key.Metasploitable-vagrant-key-pair.public_key_openssh
 }
 
-# Save file
-resource "local_file" "Metasploitable-vagrant-key-pair" {
-  filename = "${aws_key_pair.Metasploitable-vagrant-key-pair.key_name}.pem"
-  content  = tls_private_key.Metasploitable-vagrant-key-pair.private_key_pem
-}
-
 # Generates a secure private key and encodes it as PEM
 # 4 key
 resource "tls_private_key" "Windows-10-key-pair" {
@@ -68,12 +50,6 @@ resource "aws_key_pair" "Windows-10-key-pair" {
   public_key = tls_private_key.Windows-10-key-pair.public_key_openssh
 }
 
-# Save file
-resource "local_file" "Windows-10-key-pair" {
-  filename = "${aws_key_pair.Windows-10-key-pair.key_name}.pem"
-  content  = tls_private_key.Windows-10-key-pair.private_key_pem
-}
-
 # Generates a secure private key and encodes it as PEM
 # 5 key
 resource "tls_private_key" "kali-demo-key_pair" {
@@ -83,35 +59,30 @@ resource "tls_private_key" "kali-demo-key_pair" {
 
 # Create the Key Pair
 resource "aws_key_pair" "kali-demo-key_pair" {
-  key_name   = "kali-demo-key_pair"  
+  key_name   = var.keyname5  
   public_key = tls_private_key.kali-demo-key_pair.public_key_openssh
 }
 
-# Save file
-resource "local_file" "kali-demo-key_pair" {
-  filename = "${aws_key_pair.kali-demo-key_pair.key_name}.pem"
-  content  = tls_private_key.kali-demo-key_pair.private_key_pem
-}
 
 
 
 
 output "kali-soft-key" {
-  value = tls_private_key.kali-demo-key_pair.public_key_openssh
+  value = aws_key_pair.kali-demo-key_pair.key_name
 }
 
 output "window-server-key" {
-  value = tls_private_key.Windows-10-key-pair.public_key_openssh
+  value = aws_key_pair.Windows-10-key-pair.key_name
 }
 
 output "metasploitable-server-key" {
-  value = tls_private_key.Metasploitable-vagrant-key-pair.public_key_openssh
+  value = aws_key_pair.Metasploitable-vagrant-key-pair.key_name
 }
 
 output "marlinspike-key" {
-  value = tls_private_key.marlinspike-key-pair.public_key_openssh
+  value = aws_key_pair.marlinspike-key-pair.key_name
 }
 
 output "kali-server-key" {
-  value = tls_private_key.kali-without-key-pair.public_key_openssh
+  value = aws_key_pair.kali-without-key-pair.key_name
 }
