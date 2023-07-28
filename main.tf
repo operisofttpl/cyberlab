5d41823e201e9516d723dae172237015d78bc357
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-07-28 14:10:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_key_pair" "master-key-pair" {
 # Kali rdp
 resource "aws_instance" "kali_server" {
   ami           = "ami-037a1c3dbe88d5d11"  # Replace with your desired AMI ID
-  instance_type = "c6a.xlarge"  # Replace with your desired instance type
+  instance_type = "c5a.xlarge"  # Replace with your desired instance type
   key_name      = aws_key_pair.master-key-pair.key_name
   subnet_id = "subnet-0fa129c7b7544492c"
   availability_zone = "ap-south-1b"
